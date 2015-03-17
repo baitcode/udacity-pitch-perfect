82ce3bcab422a1b09c5e8f16d041af84e4608651
Ilya Batiy <ilya@ostrovok.ru>
2015-03-18 00:43:51 +0300

    added "Tap to continue" removed semilocons changed   self.reverbEffect.loadFactoryPreset(     AVAudioUnitReverbPreset.LargeChamber   ); to   self.reverbEffect.wetDryMix = 10

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -18,85 +18,90 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var darthVaderBtn: UIButton!
     @IBOutlet weak var reverbBtn: UIButton!
     
-    var engine = AVAudioEngine();
-    var playerNode = AVAudioPlayerNode();
-    var pitchEffect = AVAudioUnitTimePitch();
-    var reverbEffect = AVAudioUnitReverb();
+    var engine = AVAudioEngine()
+    var playerNode = AVAudioPlayerNode()
+    var pitchEffect = AVAudioUnitTimePitch()
+    var reverbEffect = AVAudioUnitReverb()
 
-    var record: RecordedAudio!;
+    var record: RecordedAudio!
     
     func stop(){
-        self.playerNode.stop();
+        self.playerNode.stop()
     }
 
     func play(){
-        self.stop();
-        self.engine.startAndReturnError(nil);
-        var session = AVAudioSession.sharedInstance();
-        session.overrideOutputAudioPort(AVAudioSessionPortOverride.None, error: nil);
-        self.playerNode.scheduleFile(self.record.asAVAudioFile(), atTime: nil, completionHandler: nil);
-        self.playerNode.play();
-        
+        self.stop()
+        self.engine.startAndReturnError(nil)
+        var session = AVAudioSession.sharedInstance()
+        session.overrideOutputAudioPort(AVAudioSessionPortOverride.None, error: nil)
+        self.playerNode.scheduleFile(self.record.asAVAudioFile(), atTime: nil, completionHandler: nil)
+        self.playerNode.play()
     }
     
     func resetEffects() {
-        self.pitchEffect.rate = 1;
-        self.pitchEffect.pitch = 1;
-        self.reverbEffect.wetDryMix = 0;
+        self.pitchEffect.rate = 1
+        self.pitchEffect.pitch = 1
+        self.reverbEffect.wetDryMix = 0
     }
     
     @IBAction func playSlowAction(sender: UIButton) {
-        self.resetEffects();
-        self.pitchEffect.rate = 0.5;
-        self.play();
+        self.resetEffects()
+        self.pitchEffect.rate = 0.5
+        self.play()
     }
 
     @IBAction func playReverbAction(sender: UIButton) {
-        self.resetEffects();
-        self.reverbEffect.loadFactoryPreset(
-            AVAudioUnitReverbPreset.LargeChamber
-        );
-        self.play();
+        self.resetEffects()
+        // Hi! It took me a while to realize how code review actually works
+        // so I've finally found your comments, but there were no way I could respond to your comment.
+        // So i'm writing you a message here. Yes resetEffects resets all effect settings to defaults
+        // but the next line was
+        // self.reverbEffect.loadFactoryDefault(...)
+        // I though that this method set's up wetDryMix with some predefined value. 
+        // Probably I've picked wrong one, the one that is barely heard. Anyway I've changed it according to your
+        // comment. Thank you very much for your time and please ask somebody to add reply feature 8 ).
+        self.reverbEffect.wetDryMix = 10
+        self.play()
     }
     
     @IBAction func playFastAction(sender: UIButton) {
-        self.resetEffects();
-        self.pitchEffect.rate = 1.5;
-        self.play();
+        self.resetEffects()
+        self.pitchEffect.rate = 1.5
+        self.play()
     }
     
     @IBAction func playChimpmonkAction(sender: UIButton) {
-        self.resetEffects();
-        self.pitchEffect.pitch = 1000;
-        self.play();
+        self.resetEffects()
+        self.pitchEffect.pitch = 1000
+        self.play()
     }
 
     @IBAction func playDartVaderAction(sender: UIButton) {
-        self.resetEffects();
-        self.pitchEffect.pitch = -1000;
-        self.play();
+        self.resetEffects()
+        self.pitchEffect.pitch = -1000
+        self.play()
     }
 
     @IBAction func stopAction(sender: UIButton) {
-        self.stop();
+        self.stop()
     }
         
     
     override func viewDidLoad() {
-        self.engine.reset();
-        self.engine.stop();
+        self.engine.reset()
+        self.engine.stop()
 
-        self.engine.attachNode(self.playerNode);
-        self.engine.attachNode(self.pitchEffect);
-        self.engine.attachNode(self.reverbEffect);
+        self.engine.attachNode(self.playerNode)
+        self.engine.attachNode(self.pitchEffect)
+        self.engine.attachNode(self.reverbEffect)
         
-        self.engine.connect(self.playerNode, to: self.pitchEffect, format: nil);
-        self.engine.connect(self.pitchEffect, to: self.reverbEffect, format: nil);
-        self.engine.connect(self.reverbEffect, to: self.engine.outputNode, format: nil);
+        self.engine.connect(self.playerNode, to: self.pitchEffect, format: nil)
+        self.engine.connect(self.pitchEffect, to: self.reverbEffect, format: nil)
+        self.engine.connect(self.reverbEffect, to: self.engine.outputNode, format: nil)
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning();
+        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
--- Pitch Perfect/RecordAudioViewController.swift
+++ Pitch Perfect/RecordAudioViewController.swift
@@ -11,111 +11,111 @@ import AVFoundation
 
 class RecordAudioViewController: UIViewController, AVAudioRecorderDelegate {
     
-    @IBOutlet weak var stopBtn: UIButton!;
-    @IBOutlet weak var statusLbl: UILabel!;
-    @IBOutlet weak var recordingBtn: UIButton!;
-    @IBOutlet weak var pauseBtn: UIButton!;
+    @IBOutlet weak var stopBtn: UIButton!
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var recordingBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
     
-    var recorder: AVAudioRecorder!;
+    var recorder: AVAudioRecorder!
     
-    var recordedAudio: RecordedAudio!;
+    var recordedAudio: RecordedAudio!
     
-    var recordingStarted = false;
+    var recordingStarted = false
     
     func setStatus(status: String) {
-        statusLbl.hidden = status == "";
-        statusLbl.text = status;
+        statusLbl.hidden = status == ""
+        statusLbl.text = status
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad();
+        super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
-        self.setStatus("Tap to record");
-        self.stopBtn.hidden = true;
-        self.recordingBtn.enabled = true;
-        self.pauseBtn.hidden = true;
-        self.recordingStarted = false;
+        self.setStatus("Tap to record")
+        self.stopBtn.hidden = true
+        self.recordingBtn.enabled = true
+        self.pauseBtn.hidden = true
+        self.recordingStarted = false
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning();
+        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     func startRecording(){
-        var session = AVAudioSession.sharedInstance();
+        var session = AVAudioSession.sharedInstance()
         session.setCategory(
             AVAudioSessionCategoryPlayAndRecord,
             withOptions: AVAudioSessionCategoryOptions.DefaultToSpeaker,
             error: nil
-        );
-        self.recordedAudio = RecordedAudio();
-        self.recorder = AVAudioRecorder(URL: recordedAudio.getFileUrl(), settings: nil, error: nil);
-        self.recorder.delegate = self;
-        self.recorder.meteringEnabled = true;
-        self.recorder.prepareToRecord();
-        self.recorder.record();
-        self.recordingStarted = true;
+        )
+        self.recordedAudio = RecordedAudio()
+        self.recorder = AVAudioRecorder(URL: recordedAudio.getFileUrl(), settings: nil, error: nil)
+        self.recorder.delegate = self
+        self.recorder.meteringEnabled = true
+        self.recorder.prepareToRecord()
+        self.recorder.record()
+        self.recordingStarted = true
     }
 
     func resumeRecording(){
-        self.recorder.record();
+        self.recorder.record()
     }
     
     @IBAction func record(sender: UIButton) {
-        self.recordingBtn.enabled = false;
-        self.setStatus("recording");
-        self.pauseBtn.hidden = false;
-        self.stopBtn.hidden = false;
+        self.recordingBtn.enabled = false
+        self.setStatus("recording")
+        self.pauseBtn.hidden = false
+        self.stopBtn.hidden = false
 
         if (!self.recordingStarted){
-            self.startRecording();
+            self.startRecording()
         } else {
-            self.resumeRecording();
+            self.resumeRecording()
         }
     }
     
     @IBAction func stop(sender: UIButton) {
-        self.recordingBtn.enabled = true;
-        self.setStatus("");
-        self.pauseBtn.hidden = true;
-        self.stopBtn.hidden = true;
+        self.recordingBtn.enabled = true
+        self.setStatus("")
+        self.pauseBtn.hidden = true
+        self.stopBtn.hidden = true
         
-        self.recorder.stop();
-        var session = AVAudioSession.sharedInstance();
-        session.setActive(false, error: nil);
+        self.recorder.stop()
+        var session = AVAudioSession.sharedInstance()
+        session.setActive(false, error: nil)
     }
 
     @IBAction func pause(sender: UIButton) {
-        self.recordingBtn.enabled = true;
-        self.setStatus("paused");
-        self.stopBtn.hidden = false;
-        self.pauseBtn.hidden = true;
+        self.recordingBtn.enabled = true
+        self.setStatus("Tap to continue")
+        self.stopBtn.hidden = false
+        self.pauseBtn.hidden = true
         
-        self.recorder.pause();
+        self.recorder.pause()
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if (segue.identifier == "stopRecording"){
             let playSoundsVC: PlaySoundsViewController =
-                segue.destinationViewController as PlaySoundsViewController;
-            playSoundsVC.record = sender as RecordedAudio;
+                segue.destinationViewController as PlaySoundsViewController
+            playSoundsVC.record = sender as RecordedAudio
         }
     }
     
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
         if (flag){
-            self.recordedAudio = RecordedAudio();
-            self.recordedAudio.title = recorder.url.lastPathComponent;
-            self.recordedAudio.filePathURL = recorder.url;
+            self.recordedAudio = RecordedAudio()
+            self.recordedAudio.title = recorder.url.lastPathComponent
+            self.recordedAudio.filePathURL = recorder.url
             self.performSegueWithIdentifier("stopRecording", sender: self.recordedAudio)
         } else {
-            self.statusLbl.hidden = true;
-            stopBtn.hidden = true;
-            pauseBtn.hidden = true;
-            recordingBtn.enabled = true;
+            self.statusLbl.hidden = true
+            stopBtn.hidden = true
+            pauseBtn.hidden = true
+            recordingBtn.enabled = true
         }
     }
     
